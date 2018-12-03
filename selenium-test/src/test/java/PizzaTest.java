f4b5de5c38a2ae8b00481ab550dea30a33faeec0
Jerrek <Jerrek@mail.ru>
2018-12-03 13:17:37 +0300

    Много что поправил

--- selenium-test/src/test/java/PizzaTest.java
+++ selenium-test/src/test/java/PizzaTest.java
@@ -2,27 +2,34 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
 
-@Test
-public class PizzaSushiWok extends BaseClass {
-    @Test(description = "Заказ пиццы \"Вкусная\", 40 см на pizzasushiwor.ru, ")
+
+public class PizzaTest extends BaseClass {
+    @Test(description = "Заказ пиццы \"Вкусная\", 40 см на pizzasushiwor.ru")
     public void deliveryTest() throws Exception {
         //заходим на страницу пиццы суши вока
+        System.out.println("Заходим на сайт ttps://pizzasushiwok.ru");
         driver.get("https://pizzasushiwok.ru");
         //кликаем на пиццу
+        System.out.println("Выбираем раздел пицца");
         driver.findElement(By.xpath("/html/body/div[3]/div[1]/header/div[2]/div/nav/ul/li[1]/a/span")).click();
         //кликаем на 40см
+        System.out.println("Выбираем 40 см, кто вообще берет пиццу меньше 40см?");
         driver.findElement(By.xpath("//*[@id=\"item404\"]/div[3]/div[2]/div[2]/div[3]/div[3]/div[2]")).click();
         //кликаем на добавить в корзину
+        System.out.println("Добавляем выбранное в корзину");
         driver.findElement(By.xpath("//*[@id=\"item404\"]/div[3]/div[2]/div[2]/div[7]/div/a")).click();
         Thread.sleep(10000);
         //кликаем на корзину
+        System.out.println("Заходим в корзину");
         driver.findElement(By.xpath("/html/body/div[2]/div[1]/header/div[2]/div/div/ul/li[2]/a")).click();
         //кликаем на далее
         //По причинам настройки сайта (кривыми разработчиками), кликать на кнопку далее в заказе, можно
         // только вызвав JavaScript код, завязанный на клик по этой кнопке
+        System.out.println("Кликаем на далее");
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("model_cart.goto_order();");
         //Заполняем рег. данные
+        System.out.println("Заполняем рег данные");
         driver.findElement(By.xpath("//*[@id=\"order_name\"]")).sendKeys("Marvin");
         driver.findElement(By.xpath("//*[@id=\"order_phone\"]")).sendKeys("89164214214");
         driver.findElement(By.xpath("//*[@id=\"order_metro\"]")).sendKeys("Савеловская");
@@ -32,6 +39,7 @@ public class PizzaSushiWok extends BaseClass {
         driver.findElement(By.xpath("//*[@id=\"order_code_dom_user\"]")).sendKeys("42");
         driver.findElement(By.xpath("//*[@id=\"order_floor_user\"]")).sendKeys("42");
         //кликаем на подвтвердить
+        System.out.println("Кликать на подтвердить, мы не будем, дабы не бесить операторов =)");
 //        js.executeScript("send_current_order(event); return false;");// не стоит, а то начнут звонить, всех беспокоить
     }
 }
--- selenium-test/src/test/java/YandexTest.java
+++ selenium-test/src/test/java/YandexTest.java
@@ -4,13 +4,17 @@ import org.testng.annotations.Test;
 public class YandexTest extends BaseClass {
     @Test(description = "Попытка захода на яндекс")
     public void yandexTest() {
+        System.out.println("Заходим на сайт Yandex");
         driver.get("https://www.yandex.ru");//get инструкция, для захода на переданную в метод ссылку
         //тут мы вводим текст в поле запроса, обратите внимание, что поиск элемента на странице
         //происходит по id web элемента
-        driver.findElement(By.id("text")).sendKeys("selenium webdriver xpath");
+        System.out.println("Вбиваем в строку поиска инфу");
+        driver.findElement(By.id("text")).sendKeys("selenium webdriver xpath");//sendKeys инструкция, для ввода текста
         //а вот тут, мы нашли элемент уже по имени класса
-        driver.findElement(By.className("search2__button")).click();
+        System.out.println("Кликаем по кнопке найти");
+        driver.findElement(By.className("search2__button")).click();//это клик... инструкция клик кликает, вот так: клик клик =)
         //а вот тут мы уже наконец добрались до xpath
+        System.out.println("Кликаем по нужной ссылке");
         driver.findElement(By.xpath("/html/body/div[3]/div[1]/div[2]/div[1]/div[1]/ul/li[3]/div/h2/a/div[2]")).click();
     }
 }
